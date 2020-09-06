7f3679dba4fef01df03f17e6a86ba2b389bff0fb
nas <hts.ns.b23@gmail.com>
2020-09-06 05:43:49 +0000

    favorite,unfavorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: "Relationship", foreign_key: "follow_id"
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
+  
 
   def follow(other_user)
     unless self == other_user
@@ -27,6 +31,20 @@ class User < ApplicationRecord
   def following?(other_user)
     self.followings.include?(other_user)
   end
+  
+  def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unfavorite(micropost)
+  favorite = favorites.find_by(micropost_id: micropost.id)
+  favorite.destroy if favorite
+  end
+  
+  def already_favorite?(micropost)
+    self.likes.include?(micropost)
+  end
+  
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
